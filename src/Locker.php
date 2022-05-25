15f7d224edfaddd5d46eabf5d9fa42f51c90672f
yuanzhihai <396751927@qq.com>
2022-05-25 09:47:18 +0800

    init

--- src/Locker.php
+++ src/Locker.php
@@ -7,7 +7,7 @@ use Symfony\Component\Lock\LockFactory;
 use Symfony\Component\Lock\LockInterface;
 
 /**
- * @method static LockInterface create(?string $orderId = null, ?float $ttl = null, ?bool $autoRelease = null, ?string $prefix = null)
+ * @method static LockInterface lock(?string $orderId = null, ?float $ttl = null, ?bool $autoRelease = null, ?string $prefix = null)
  */
 class Locker
 {
@@ -17,7 +17,7 @@ class Locker
     {
         $key = $arguments[0] ?? '';
         unset($arguments[0]);
-        return static::createLock($name . $key, ...$arguments);
+        return static::createLock($key, ...$arguments);
     }
 
     /**
